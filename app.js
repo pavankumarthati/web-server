45bf417ac8ba74045604c903c492d83fcfc83021
pavan kumar <pavankumar.thati@gmail.com>
2017-01-18 21:26:26 +0530

    update port for heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = 3000; // it is constant if specified in uppercase
+var PORT = process.env.PORT || 3000; // it is constant if specified in uppercase
 
 var middleware = require('./middleware');
 app.use(middleware.logger); // app level middleware
